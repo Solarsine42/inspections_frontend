298f57fb19d46a673c1003dc04680c670ff45dad
Solarsine42 <Solarsine42@gmail.com>
2020-03-03 20:59:18 -0700

    del archive updated

--- src/components/archived/DeleteArchive.js
+++ src/components/archived/DeleteArchive.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import { deleteArchive } from "../../store/archived/actions";
-import Loading from "../utility/Loading";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Slide from "@material-ui/core/Slide";
 import DeleteIcon from "@material-ui/icons/Delete";
