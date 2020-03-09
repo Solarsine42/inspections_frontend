cc019db623ffae4da7503cb2a6d63c5bcac61023
Solarsine42 <Solarsine42@gmail.com>
2020-03-09 14:43:14 -0700

    home decor

--- src/components/utility/Home.js
+++ src/components/utility/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Home = () => {
-  return null;
+  return (
+    <div id="homePage">
+      <div id="homeText">
+        <p>
+          Welcome to your Hub of all things ITV! On this site, you will be able
+          to start and edit Homeowners Inspections, view archives related to a
+          member, and request review from an Underwriter if the member states
+          there's a discrepancy in the home characteristics.
+        </p>
+        <p>
+          Utilize the search field in the upper-right-hand corner to refine your
+          search and your options to a particular member. Utilize the Menu
+          button in the upper-left-hand corner for commonly use KD references,
+          or to submit a feature suggestion to the creator.
+        </p>
+        <p>Click on any of the three links above to begin.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
